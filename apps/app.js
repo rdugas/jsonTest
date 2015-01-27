ad9dd97c9aa4b0fb70f8f39a40bb6bea334467fa
Rob Dugas <rob.dugas@gmail.com>
2015-01-26 22:48:33 -0600

    refactor request method

--- apps/app.js
+++ apps/app.js
@@ -1,31 +1,32 @@
 $(document).ready(function() {
-
-    
     $("#search-term").submit(function(event) {
-    	
-    	console.log("button hit");
-    	console.log($('#query').val());
 
-    	event.preventDefault();
-    	var searchTerm = $('#query').val();
-    	var searchString = 'https://www.omdbapi.com/?s=' + searchTerm + '&r=json';
-    	console.log(searchString);
-    
-    	$.getJSON(('http://www.omdbapi.com/?s=' + searchTerm + '&r=json'), function(data) {
+        console.log("button hit");
+        console.log($('#query').val());
+
+        event.preventDefault();
+        var searchTerm = $('#query').val();
+        var searchString = 'http://www.omdbapi.com/?s=' + searchTerm + '&r=json';
+        console.log(searchString);
+        getRequest(searchString);
+
+    });
+});
+
+function getRequest(searchString) {
+    $.getJSON(searchString, function(data) {
         console.log("get json called");
         console.log(data.Search);
         showResults(data.Search);
-    	});
-		
-	});
+    });
+}
 
-    function showResults(results) {
-    	var html = "";
-        $.each(results, function(index, value) {
-            html += '<p>' +  value.Title + '</p>';
-            console.log(value.Title);
-        });
-        $('#search-results').html(html);
 
-    }
-});
+function showResults(results) {
+    var html = "";
+    $.each(results, function(index, value) {
+        html += '<p>' + value.Title + '</p>';
+        console.log(value.Title);
+    });
+    $('#search-results').html(html);
+}
