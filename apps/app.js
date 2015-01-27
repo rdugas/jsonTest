2a2e22dced6e7edf429c56003e05248ee3874bc9
Rob Dugas <rob.dugas@gmail.com>
2015-01-26 22:55:18 -0600

    refactor get request to build params and setup for future expansion

--- apps/app.js
+++ apps/app.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
         console.log($('#query').val());
 
         event.preventDefault();
-        var searchTerm = $('#query').val();
-        var searchString = 'http://www.omdbapi.com/?s=' + searchTerm + '&r=json';
+        var searchString = $('#query').val();
+        // var searchString = 'http://www.omdbapi.com/?s=' + searchTerm + '&r=json';
         console.log(searchString);
         getRequest(searchString);
 
@@ -14,7 +14,13 @@ $(document).ready(function() {
 });
 
 function getRequest(searchString) {
-    $.getJSON(searchString, function(data) {
+    var params = {
+        s: searchString,
+        r: 'json'
+    };
+    url = 'http://www.omdbapi.com';
+
+    $.getJSON(url, params, function(data) {
         console.log("get json called");
         console.log(data.Search);
         showResults(data.Search);
